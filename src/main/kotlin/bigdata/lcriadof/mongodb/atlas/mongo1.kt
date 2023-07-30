60e4cfd5849a7116e9b6223f2527fa9cf21a4f4d
LUIS CRIADO FERNANDEZ <lcriadof@yahoo.es>
2023-07-30 09:15:52 +0200

    Create mongo1.kt

---
+++ src/main/kotlin/bigdata/lcriadof/mongodb/atlas/mongo1.kt
@@ -0,0 +1,35 @@
+package bigdata.lcriadof.mongodb.atlas
+
+
+import org.bson.Document
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.ServerApi
+import com.mongodb.ServerApiVersion
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import kotlinx.coroutines.runBlocking
+
+
+    fun main() {
+        // Replace the placeholders with your credentials and hostname
+        val connectionString = "mongodb+srv://luiscriadofernandez:fnYCwT6OryjkCPnO@cluster0.szjjj5q.mongodb.net/?retryWrites=true&w=majority"
+
+        val serverApi = ServerApi.builder()
+            .version(ServerApiVersion.V1)
+            .build()
+
+        val mongoClientSettings = MongoClientSettings.builder()
+            .applyConnectionString(ConnectionString(connectionString))
+            .serverApi(serverApi)
+            .build()
+
+        // Create a new client and connect to the server
+        MongoClient.create(mongoClientSettings).use { mongoClient ->
+            val database = mongoClient.getDatabase("admin")
+            runBlocking {
+                database.runCommand(Document("ping", 1))
+            }
+            println("Pinged your deployment. You successfully connected to MongoDB!")
+        }
+    }
+
